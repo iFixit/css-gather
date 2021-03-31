faf504a0a7fe79e66f95293d4ad5eec793e9d0d9
Andrew Gilbert <andrewg@ifixit.com>
2021-03-31 15:46:01 -0700

    Add script to generate critical CSS for a URL

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+if [[ -z "${1:-}" ]]
+then
+  echo "Usage: run.sh <url>"
+  exit 1
+fi
+
+root="$(dirname "$0")"
+
+"$root/css-gather" "$1" | "$root/critical-css.js" "$1"
