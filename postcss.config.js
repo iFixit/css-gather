4f2e2bfcab2be954b04618916e7730bf7e2ba8c0
Andrew Gilbert <andrewg@ifixit.com>
2021-04-20 18:25:16 -0700

    Add postcss config
    We'll use postcss to clean up combined CSS files. It can remove
    duplicated selectors and sort our properties (to make diffs cleaner).

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('postcss-combine-duplicated-selectors')({removeDuplicatedValues: true}),
+    require('postcss-sorting')({"unspecified-properties-position": "bottomAlphabetical"})
+  ],
+}
