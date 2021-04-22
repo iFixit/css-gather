76acc3fe31db8fea71ebadc214ec2631ba2579e3
Andrew Gilbert <andrewg@ifixit.com>
2021-04-21 17:57:11 -0700

    Prettify config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
-    require('postcss-combine-duplicated-selectors')({removeDuplicatedValues: true}),
-    require('postcss-sorting')({"unspecified-properties-position": "bottomAlphabetical"})
+    require("postcss-combine-duplicated-selectors")({
+      removeDuplicatedValues: true,
+    }),
+    require("postcss-sorting")({
+      "unspecified-properties-position": "bottomAlphabetical",
+    }),
   ],
-}
+};
