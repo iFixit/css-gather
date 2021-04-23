c5b6a58bf2ecb5bfa3b6b3e7a28066c09452f900
Andrew Gilbert <andrewg@ifixit.com>
2021-04-22 17:41:18 -0700

    postcss: Drop `combine-duplicated-selectors`
    It seems that this plugin doesn't necessarily make only safe
    transformations. `cssnano` seems to do an adequate job in `advanced`
    mode, so let's get rid of this one.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [
-    require("postcss-combine-duplicated-selectors")({
-      removeDuplicatedValues: true,
-    }),
     require("cssnano")({ preset: "advanced" }),
     require("postcss-sorting")({
       "unspecified-properties-position": "bottomAlphabetical",
