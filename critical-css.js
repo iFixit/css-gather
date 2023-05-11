80656ab3c9a33edfdca9ffa7b77dbeebd4eb3746
batbattur <bat@ifixit.com>
2023-05-11 12:02:31 -0700

    Update the forceInclude regex to be specific
    This new regex will match selectors like the following:
    :where(), :has(), :not(), :host(), :root().
    
    Verified that it will not match selectors like the following:
    li(:last-child), .green:not(:disabled), .card-hover:hover.

--- critical-css.js
+++ critical-css.js
@@ -46,7 +46,7 @@ function findCriticalCss(cssString, url) {
     height: 2160,
     blockJSRequests: false,
     forceExclude: excludeArr,
-    forceInclude: [/where/i],
+    forceInclude: [/^:(\w+)\(/i],
     screenshots: {
       basePath: getScreenshotPath(url),
       type: 'jpeg',
